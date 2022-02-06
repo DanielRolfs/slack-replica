e76056cf24f4f5766b62bd1566a36b207fe71c9f
Daniel Rolfs <danielrolfs@icloud.com>
2022-02-06 18:11:27 +0100

    fix: login folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login/login.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register/register.component';
 import { PasswordresetComponent } from './login/passwordreset/passwordreset.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
